4ddf03e27690c9f16835ffad16f56326875eb6cb
AdrianoA <adriano.armendaris@gmail.com>
2021-07-29 04:02:52 -0500

    bootstrapped page structure and id assignation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
+// API Key: 840155a469a1270ff82a976016c38d58
+
 // variables 
 let cityStored = [];
 let searchForm = $("#search-form");
 let searchInput = $("#search-input");
 let selectedWeatherCont = $("#selected-weather-container");
 let selectedCity = $("#selected-city")
+let predictionH = $("#prediction");
+let fiveContainer = $("#five-container");
+let storedSearches = $("#stored-searches");
+
