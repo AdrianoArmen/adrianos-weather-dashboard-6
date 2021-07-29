a238cd175fd9d0057547e8d4ea6b9ce98554d332
AdrianoA <adriano.armendaris@gmail.com>
2021-07-29 03:18:20 -0500

    basci variables declared and id matched on html elements

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// variables 
+let cityStored = [];
+let searchForm = $("#search-form");
+let searchInput = $("#search-input");
+let selectedWeatherCont = $("#selected-weather-container");
+let selectedCity = $("#selected-city")
