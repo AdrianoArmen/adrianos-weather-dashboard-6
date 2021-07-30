f87165bdebc48f4aec488abdd5069a79526ee40f
AdrianoA <adriano.armendaris@gmail.com>
2021-07-29 23:55:18 -0500

    added fetch weather api function

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,21 @@ let predictionH = $("#prediction");
 let fiveContainer = $("#five-container");
 let storedSearches = $("#stored-searches");
 
+
+//  openweather fetch api using api key
+
+let cityWeather = function (city) {
+    console.log("cityWeather");
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=840155a469a1270ff82a976016c38d58`
+    )
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            displayW(data, town);
+        });
+};
+
